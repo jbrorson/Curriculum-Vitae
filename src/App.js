ed9405b003e9adee59904c60fb867fa77c157860
jbrorson <josefine.brorson@gmail.com>
2020-06-25 16:55:47 +0200

    first push

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Home } from './components/Home';
+import { About } from './components/About';
+import { NoMatch } from './components/NoMatch';
+
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavigationBar } from './components/NavigationBar';
+
+// const Appstyle = styled.h1`
+// margin: auto; 
+// display: flex; 
+// justify-content: center;
+// background: darkcyan;
+// color: whitesmoke;
+// `
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <React.Fragment>
+      <Router>
+        <NavigationBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/about" component={About} />
+          <Route component={NoMatch} />
+        </Switch>
+      </Router>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,60 @@
+import React from 'react'; 
+import styled from 'styled-components';
+import { Nav, Navbar, Form, FormControl } from 'react-bootstrap';
+
+
+const Styles = styled.div`
+  .navbar { background-color: #222; }
+  a, .navbar-nav, .navbar-light .nav-link {
+    color: #9FFFCB;
+    &:hover { color: white; }
+  }
+  .navbar-brand {
+    font-size: 1.4em;
+    color: #9FFFCB;
+    &:hover { color: white; }
+  }
+  .form-center {
+    position: absolute !important;
+    left: 25%;
+    right: 25%;
+  }
+`;
+
+
+export const NavigationBar = () => (
+  <Styles>
+    <Navbar expand="lg">
+      <Navbar.Brand href="/">Josefine Brorson</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+      <Form className="form-center">
+        <FormControl type="text" placeholder="Search" className="" />
+      </Form>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="ml-auto">
+          <Nav.Item><Nav.Link href="/">Home</Nav.Link></Nav.Item> 
+          <Nav.Item><Nav.Link href="/about">About</Nav.Link></Nav.Item>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  </Styles>
+)
+
+// const NavStyle = styled.nav`
+// display: flex; 
+
+// margin-top: auto 0;
+// margin-bottom: 2rem;
+// background: pink;
+// `
+
+// function Navbar() {
+//   return (
+// <NavStyle>
+//   <h5>About</h5>
+//   <h5>Location</h5>
+// </NavStyle>
+//   )
+// };
+
+export default Navbar;
